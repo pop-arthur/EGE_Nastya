8c043e679217a084e9b0d7e28e83cbce6a33d3a4
popov <artu.200505@gmail.com>
2023-09-27 18:51:45 +0300

    27_09

--- 27_09_23/1.py
+++ 27_09_23/1.py
@@ -1,1 +1,15 @@
-def f():
+def f(a, x, y):
+    return ((x <= 9) <= (x * x <= a)) and ((y * y <= a) <= (y <= 9))
+
+
+for a in range(1000):
+    flag = True
+    for x in range(100):
+        for y in range(100):
+            if not f(a, x, y):
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
